b2fa1a764033fa53fd2638d7016255f4f6eb97e2
SunHyongKwon <mikekwon42@gmail.com>
2022-09-19 16:10:29 +0900

    add main

---
+++ src/com/javalec/base/Main.java
@@ -0,0 +1,39 @@
+package com.javalec.base;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		boolean exit = true;
+		int functionNum , inputNum1 , inputNum2;
+		String result = null;
+		
+		
+		while(exit) {
+			System.out.println("1.덧셈");
+			System.out.println("2.뺄셈");
+			System.out.println("3.곱셈");
+			System.out.println("4.나눗셈 몫");
+			System.out.println("5.나눗셈 나머지");
+			System.out.println("6.나눗셈");
+			System.out.println("7.종료");
+			
+			System.out.print(">>번호를 선택하세요 : ");
+			functionNum = sc.nextInt();
+			
+			System.out.print("첫번째 숫자를 입력하세요 : ");
+			inputNum1 = sc.nextInt();
+			System.out.print("두번째 숫자를 입력하세요 : ");
+			inputNum2 = sc.nextInt();
+			
+			// result 에 번호 선택한 결과 넣어서 이걸 프린트 하면 됨
+		}
+		
+		
+		
+	}
+
+}
